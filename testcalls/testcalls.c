06c5356b3a4eab05729d7e8121df72b92e8637b9
mosaicthej <mij623@usask.ca>
2024-01-03 09:25:30 -0600

    add test multiple calls

---
+++ testcalls/testcalls.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int inputNumber() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  return num;
+}
+
+int addTen(int num) {
+  return num + 10;
+}
+
+void printNumber(int num) {
+  printf("Number plus 10 is %d\n", num);
+}
+
+int main() {
+  int num = inputNumber();
+  num = addTen(num);
+  printNumber(num);
+  return 0;
+}
+
